bd74ebf03895f29292e66f52ff1ea7e4a4a7f17c
xjh <296894139@qq.com>
2020-07-27 14:25:20 +0800

    Update CodeScanner.py

--- CodeScanner.py
+++ CodeScanner.py
@@ -1,6 +1,8 @@
 class CodeScanner():
     def __init__(self,filepath):
+
         self.filepath=filepath
+
         self.methodsContained={
             "__init__":0,
             "__construct":0,
@@ -11,12 +13,21 @@ class CodeScanner():
             "save":0,
             "monitor":0
         }
-        self.otherMethods=[]
-    def pylint(self):
-        from pylint.lint import Run
-        Run([self.filepath])
 
+        self.propertiesContained={
+            "train_parser":0,
+            "eval_parser":0
+        }
+
+        # methods not included in our design file
+        self.otherMethods=[]
+
+    def pylint(self):
+        # rate the code
+        from pylint.lint import Run
+        Run([self.filepath],do_exit=False)
     def extractFuns(self):
+        # extract all functions from the file,return the start and end line of a function
         with open(self.filepath,'r') as f:
             lines=f.readlines()
             funs=[[]]
@@ -42,6 +53,7 @@ class CodeScanner():
             return funs
 
     def memberMethodsCheck(self):
+        #check all the methods in py file
         with open(self.filepath,'r') as f:
             lines=f.readlines()
             funs=self.extractFuns()
@@ -59,10 +71,25 @@ class CodeScanner():
                     line=lines[begin]
                     self.otherMethods.append(line.replace("def","").strip()[:-1])
 
+    def memberPropertyCheck(self):
+        with open(self.filepath,'r') as f:
+            lines=f.readlines()
+            funs=self.extractFuns()
+            for fun in funs:
+                begin=fun[0]
+                end=fun[1]
+                if("__init__" in lines[begin]):
+                    for i in range(begin,end+1):
+                        if("train_parser" in lines[i]):
+                            self.propertiesContained["train_parser"]=1
+                        if("eval_parser" in lines[i]):
+                            self.propertiesContained["eval_parser"]=1
+
 c=CodeScanner("kmeans.py")
-c.pylint()
 c.memberMethodsCheck()
+c.memberPropertyCheck()
+c.pylint()
 print(c.methodsContained)
 print(c.otherMethods)
-
+print(c.propertiesContained)
 
