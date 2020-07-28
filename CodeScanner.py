88acb9d7282cb2040200a66e4ec4b509001708b2
xjh <296894139@qq.com>
2020-07-28 08:32:13 +0800

    Update CodeScanner.py

--- CodeScanner.py
+++ CodeScanner.py
@@ -4,14 +4,14 @@ class CodeScanner():
         self.filepath=filepath
 
         self.methodsContained={
-            "__init__":0,
-            "__construct":0,
-            "load":0,
-            "train":0,
-            "predict":0,
-            "eval":0,
-            "save":0,
-            "monitor":0
+            "__init__(self)":0,
+            "__construct(self,**build_args_kw)":0,
+            "load(self,path)":0,
+            "train(self,train_parser)":0,
+            "predict(self,path)":0,
+            "eval(self,eval_parser)":0,
+            "save(self,path,construct_args)":0,
+            "monitor(self)":0
         }
 
         self.propertiesContained={
@@ -26,13 +26,13 @@ class CodeScanner():
         # rate the code
         from pylint.lint import Run
         Run([self.filepath],do_exit=False)
-    def extractFuns(self):
+    def _extractFuns(self):
         # extract all functions from the file,return the start and end line of a function
         with open(self.filepath,'r') as f:
             lines=f.readlines()
             funs=[[]]
             begin=-1
-            spaces=-1
+            space=-1
             for index,line in enumerate(lines):
                 if(begin!=-1):
                     tem_space=0
@@ -48,6 +48,8 @@ class CodeScanner():
                     while(line[tem_space]==' '):
                         tem_space=tem_space+1
                     space=tem_space
+            if(begin!=-1):
+                funs.append([begin,len(lines)-1])
             assert (len(funs)>=1)
             funs.remove([])
             return funs
@@ -56,13 +58,13 @@ class CodeScanner():
         #check all the methods in py file
         with open(self.filepath,'r') as f:
             lines=f.readlines()
-            funs=self.extractFuns()
+            funs=self._extractFuns()
             for fun in funs:
 
                 begin=fun[0]
                 other=True
                 for method in self.methodsContained:
-                    if(method in lines[begin].split("(")[0]):
+                    if(method == lines[begin].replace("def ","").replace(":","").replace(" ","").strip()):
                         other=False
                         self.methodsContained[method]=1
                         break
@@ -74,22 +76,24 @@ class CodeScanner():
     def memberPropertyCheck(self):
         with open(self.filepath,'r') as f:
             lines=f.readlines()
-            funs=self.extractFuns()
+            funs=self._extractFuns()
             for fun in funs:
                 begin=fun[0]
                 end=fun[1]
-                if("__init__" in lines[begin]):
+                if("__init__(self)" in lines[begin]):
                     for i in range(begin,end+1):
                         if("train_parser" in lines[i]):
                             self.propertiesContained["train_parser"]=1
                         if("eval_parser" in lines[i]):
                             self.propertiesContained["eval_parser"]=1
 
-c=CodeScanner("kmeans.py")
+c=CodeScanner("test.py")
 c.memberMethodsCheck()
 c.memberPropertyCheck()
 c.pylint()
+
 print(c.methodsContained)
 print(c.otherMethods)
 print(c.propertiesContained)
+
 
